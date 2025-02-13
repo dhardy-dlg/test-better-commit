544323562d4bc44bf6839b9531d68acb23d1db93
dhardy <dhardy@novaa-tech.com>
2025-02-13 11:46:16 +0100

    feat(app): dev-002 Change default doc endpoints and add a title
    Configure swagger / redoc
    
    Closes: dev-002

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
 from fastapi import FastAPI
 
-app = FastAPI()
+app = FastAPI(
+    title="My Custom API",
+    docs_url="/custom-docs",  # Custom URL for Swagger UI
+    redoc_url="/custom-redoc",  # Custom URL for ReDoc
+)
 
 # app
 @app.get("/")
